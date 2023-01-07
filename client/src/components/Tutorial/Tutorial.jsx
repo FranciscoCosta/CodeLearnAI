3b515ea33aaea63de588733b9a234290165e0720
Xico <francisco100eg@gmail.com>
2023-01-07 10:57:32 -0300

    Fix: New Style added to Header and Hero section

--- client/src/components/Tutorial/Tutorial.jsx
+++ client/src/components/Tutorial/Tutorial.jsx
@@ -4,7 +4,9 @@ import './Tutorial.scss'
 function Tutorial() {
   return (
     <div className='Tutorial' id='Tutorial'>
-      
+      <div className="Tutorial__container">
+        <h1>Como usar CodeLearnAi ?</h1>
+      </div>
     </div>
   )
 }
